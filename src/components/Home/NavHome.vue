255908c7a84ccb031a3e5111afcde0244d2ab670
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-11-30 12:22:15 +0100

    added user bottons to HomePage

---
+++ src/components/Home/NavHome.vue
@@ -0,0 +1,61 @@
+<template>
+  <nav class="nav__user-area">
+    <button class="nav__button nav__button--login">LOG <span>IN</span></button>
+    <button class="nav__button nav__button--signup">
+      SIGN <span>UP</span>
+    </button>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "UserArea",
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style lang="scss">
+@import "@/styles/_variables";
+
+.nav__user-area {
+  display: flex;
+  position: fixed;
+}
+.nav__button {
+  width: 100px;
+  height: 46px;
+  position: absolute;
+  top: 14px;
+  opacity: 0.75;
+  border: 1px solid #595959;
+  box-sizing: border-box;
+  border-radius: 15px;
+  font-family: $Lato;
+  font-weight: 300;
+  font-size: 18px;
+  line-height: 22px;
+  text-align: center;
+  letter-spacing: 0.1em;
+  transform: scale(0.8);
+
+  &--login {
+    background-color: $light-color-home;
+    color: $dark-color-home;
+    left: 14px;
+  }
+  &--signup {
+    background-color: $dark-color-home;
+    color: $light-color-home;
+    left: 116px;
+  }
+  span {
+    color: $theme-pink;
+    font-style: italic;
+    font-weight: 500;
+  }
+}
+</style>
