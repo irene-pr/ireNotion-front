82e3339696e5205171dfdc38891f40ef53f3cd88
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-11-30 12:59:03 +0100

    added Login page with a heading

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="login-page">
+    <h1>This is the login!!! Woooooooo!!!</h1>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Login",
+});
+</script>
+
+<style lang="scss">
+@import "@/styles/_variables";
+</style>
