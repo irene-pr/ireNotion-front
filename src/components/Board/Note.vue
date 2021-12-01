74672e25018279655928b69386b956b8f9893690
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-12-01 22:29:43 +0100

    added Note component and implemented in Board

---
+++ src/components/Board/Note.vue
@@ -0,0 +1,20 @@
+<template>
+  <article class="note">
+    {{ note.id }}
+  </article>
+</template>
+<script>
+import { defineComponent } from "vue";
+import { Note } from "@/types/interfaces";
+
+export default defineComponent({
+  name: "Note",
+  props: { note: Note },
+  methods: {
+    log(event) {
+      console.log(event);
+    },
+  },
+});
+</script>
+<style lang="scss" scoped></style>
