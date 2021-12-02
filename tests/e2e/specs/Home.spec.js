7aaa6d1fae138a66acfc4f3448994e6449e3a1ac
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-12-02 21:27:09 +0100

    added Home cypress test

---
+++ tests/e2e/specs/Home.spec.js
@@ -0,0 +1,23 @@
+describe("Given a Home view", () => {
+  describe("When it goes to the Home page", () => {
+    it("Then there are 2 headers", () => {
+      cy.visit("http://localhost:8080/");
+      cy.get("h1").should("have.length", 2);
+    });
+    it("Then the 2 headers have text 'IRENOTION'", () => {
+      cy.visit("http://localhost:8080/");
+      cy.get("h1").first().should("have.text", "IRENOTION");
+      cy.get("h1").last().should("have.text", "IRENOTION");
+    });
+    it("Then there are 2 buttons", () => {
+      cy.visit("http://localhost:8080/");
+      cy.get("button").should("have.length", 2);
+    });
+    it("Then if you click the login button it goes to the login page", () => {
+      cy.visit("http://localhost:8080/");
+      cy.get("button").first().click();
+
+      cy.location("pathname").should("include", "login");
+    });
+  });
+});
