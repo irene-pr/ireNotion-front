75de4093d36f1fa93c495d75327533c89daf90c3
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-12-07 15:12:49 +0100

    added getters file

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,12 @@ import { createStore } from "vuex";
 import actions from "./actions";
 import mutations from "./mutations";
 import state from "./state";
+import getters from "./getters";
 
 export default createStore({
   state,
   mutations,
   actions,
-  getters: {},
+  getters,
   modules: {},
 });
