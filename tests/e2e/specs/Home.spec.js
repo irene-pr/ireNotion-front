c22872dbf6b36bbfa926371cc5bf7375d5a619fb
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2022-05-20 17:43:05 +0200

    cleaned unnecessary tests and separated cypress.env

--- tests/e2e/specs/Home.spec.js
+++ tests/e2e/specs/Home.spec.js
@@ -1,34 +1,23 @@
-const url = Cypress.env("PRODUCTION_URL");
-
 describe("Given a Home view", () => {
   describe("When it goes to the Home page", () => {
-    it("Then there are 2 headers", () => {
-      cy.visit(url);
-      cy.get("h1").should("have.length", 2);
-    });
-    it("Then the 2 headers have text 'IRENOTION'", () => {
-      cy.visit(url);
-      cy.get("h1").first().should("have.text", "IRENOTION");
-      cy.get("h1").last().should("have.text", "IRENOTION");
-    });
     it("Then there are 2 buttons", () => {
-      cy.visit(url);
+      cy.visit("/");
       cy.get("button").should("have.length", 2);
     });
   });
 
   describe("When you click on the login button", () => {
     it("Then it goes to the login page", () => {
-      cy.visit(url);
-      cy.get("button").first().click();
+      cy.visit("/");
+      cy.get("button.nav__button--login").click();
 
       cy.location("pathname").should("equal", "/login");
     });
   });
   describe("When you click on the sign up button", () => {
     it("Then it goes to the register page", () => {
-      cy.visit(url);
-      cy.get("button").last().click();
+      cy.visit("/");
+      cy.get("button.nav__button--signup").click();
 
       cy.location("pathname").should("equal", "/register");
     });
